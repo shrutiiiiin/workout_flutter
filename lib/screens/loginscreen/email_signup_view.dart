b7253374c11e366d6ad8ca69c31d169efad31689
Shruti Naik <111608883+shrutiiiiin@users.noreply.github.com>
2025-02-06 12:57:40 +0530

    Update email sign up

--- lib/screens/loginscreen/email_signup_view.dart
+++ lib/screens/loginscreen/email_signup_view.dart
@@ -2,13 +2,60 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class EmailSignupView extends StatelessWidget {
+class EmailSignupView extends StatefulWidget {
   const EmailSignupView({super.key});
+
+  @override
+  State<EmailSignupView> createState() => _EmailSignupViewState();
+}
+
+class _EmailSignupViewState extends State<EmailSignupView> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Create an account',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Text(
+                'Join us and start your fitness journey with Pump&Pulse',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey),
+              ),
+              const SizedBox(height: 30),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: _nameController,
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
