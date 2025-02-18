bd0dff4735c1db2a7bb408e7b2000daf001af2b5
Shruti Naik <111608883+shrutiiiiin@users.noreply.github.com>
2025-02-18 23:30:04 +0530

    update

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
 
   @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final String userId = FirebaseAuth.instance.currentUser!.uid;
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+      ),
+      body: Column(
+        children: [CircleAvatar()],
+      ),
+    );
   }
 }
